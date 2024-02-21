2c3b75238f27916a7f5a229bc17f4eeb1f75603f
Aapple21 <aapplegate12@icloud.com>
2024-02-20 20:55:15 -0600

    I believe I am finished

--- app.js
+++ app.js
@@ -22,22 +22,44 @@ app.get("/", (req, res) => {
 // http://localhost:3000/greet?name=kaylee&dob=2002
 app.get('/greet', (req, res)=> {
     console.log(req.query)
-
-    res.send(`hey, ${req.query.name}`)
+   
+    res.send(`Hey, ${req.query.name} <br/> You are ${2022 - req.query.year} or ${2023 - req.query.year} years old`)
 })
 
 app.get('/math/:num1/:op/:num2', (req, res)=> {
     console.log( req.params )
-    //res.send(`${req.params.num1}`)
-    //res.json( {message: "Hey Dude"} )
-    res.render( 'math', {title:'math', result: 5})
+    const num1 = parseInt(req.params.num1)
+    const num2 = parseInt(req.params.num2)
+    const operation = req.params.op
+    let result = 0
+
+    if(operation == 'plus'){
+        result = num1+num2
+    }
+    if (operation == 'subtract'){
+        result = num1-num2
+    }
+    if (operation == 'multiply'){
+        result = num1*num2 
+    }
+    if (operation == 'divide'){
+        result = num1/num2
+    }
+
+   
+
+    // res.send(`${req.params.num1}`)
+    // res.json( {message: "Hey Dude"} )
+    res.render( 'math', {title:'math', result: result})
 })
 
 app.get('/pandorasbox', (req, res)=> {
 
     // do the work
-
-    fetch("https://icanhazdadjoke.com/", { 
+    const coin = Math.floor(Math.random() * 2)
+   
+    if(coin == 0){
+        fetch("https://icanhazdadjoke.com/", { 
         headers: {
             "Accept": "application/json"
         }
@@ -47,12 +69,20 @@ app.get('/pandorasbox', (req, res)=> {
             console.log(data)
             res.render('pandorasbox', {title: "Pandora's Box", message: data.joke} )
         })
+        console.log(coin)
+    }
+    else{
+        // const message = "DAD JOKE"
+        const length = facts.length;
+        const random =  Math.floor( Math.random() * length)
+        const fact4 = facts[random].fact
 
-    //const message = "DAD JOKE"
-    // const length = facts.length;
-    // const random =  Math.floor( Math.random() * length)
-    // const fact4 = facts[random].fact
+        res.render('pandorasbox', {title: "Pandora's Box", message:fact4} )
+        console.log(coin)
+    }
 
-    // res.render('pandorasbox', {title: "Pandora's Box", message:fact4} )
+    
+
+    
 
 })
